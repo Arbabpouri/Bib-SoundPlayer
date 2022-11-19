fd843b7dd7d2e5f85c1b155ef3fb3cc73f62ebd4
Sardar <88272644+Sardarcybery@users.noreply.github.com>
2022-11-19 15:45:59 +0330

    Add files via upload

---
+++ Beep.py
@@ -0,0 +1,46 @@
+# in the name of god | Github : github.com/sardarcybery | Telegram : t.me/SardarCybery
+
+from random import randint
+from winsound import Beep
+
+class Bib:
+   def __init__(self,Number) -> None:
+      """
+      Args:
+          Number (int): Number of play
+      """
+      self.number = Number
+
+   def PlayBeep(self,Freq,Time):
+      """
+      Args:
+          Freq (int): Sound frequent
+          Time (int): Time for play | MS
+      """
+      if isinstance(Freq,int) and isinstance(Time,int) and isinstance(self.number,int) and self.number > 0:
+         print('Start')
+         try:
+            while self.number > 0:
+               Beep(Freq,Time)
+               self.number -= 1
+            print('Ended')
+         except KeyboardInterrupt:
+            print('you got out')
+         except Exception as ex:
+            print(str(ex))
+      else:
+         print('The arguments are not correct')
+   
+   def PlayMusic(self):
+      if isinstance(self.number,int) and self.number > 0:
+         try:
+            while self.number > 0:
+               Beep(randint(100,1200), randint(300,500))
+               self.number -= 1
+            print('Ended')
+         except KeyboardInterrupt:
+            print('you got out')
+         except Exception as ex:
+            print(str(ex))
+      else:
+         print('Number not is int')
