c5af8ea3075ee77a156a54a1d1630e73e9dca23b
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-08-25 09:26:51 +0330

    update README.md

--- Beep.py
+++ Beep.py
@@ -1,46 +1,53 @@
-# in the name of god | Github : github.com/Arbabpouri | Telegram : t.me/SardarCybery
-
 from random import randint
 from winsound import Beep
 
 class Bib:
-   def __init__(self,Number) -> None:
-      """
-      Args:
-          Number (int): Number of play
-      """
-      self.number = Number
+    def __init__(self, number: int) -> None:
+        """
+        Initialize the Bib class with the number of plays.
 
-   def PlayBeep(self,Freq,Time):
-      """
-      Args:
-          Freq (int): Sound frequent
-          Time (int): Time for play | MS
-      """
-      if isinstance(Freq,int) and isinstance(Time,int) and isinstance(self.number,int) and self.number > 0:
-         print('Start')
-         try:
-            while self.number > 0:
-               Beep(Freq,Time)
-               self.number -= 1
-            print('Ended')
-         except KeyboardInterrupt:
-            print('you got out')
-         except Exception as ex:
-            print(str(ex))
-      else:
-         print('The arguments are not correct')
-   
-   def PlayMusic(self):
-      if isinstance(self.number,int) and self.number > 0:
-         try:
-            while self.number > 0:
-               Beep(randint(100,1200), randint(300,500))
-               self.number -= 1
-            print('Ended')
-         except KeyboardInterrupt:
-            print('you got out')
-         except Exception as ex:
-            print(str(ex))
-      else:
-         print('Number not is int')
+        Args:
+            number (int): Number of plays.
+        """
+        self.number = number
+
+    def play_beep(self, freq: int, time: int) -> None:
+        """
+        Play a beep sound a specified number of times.
+
+        Args:
+            freq (int): Sound frequency.
+            time (int): Duration for which the sound plays (in milliseconds).
+        """
+        if isinstance(freq, int) and isinstance(time, int) and isinstance(self.number, int) and self.number > 0:
+            print('Start')
+            try:
+                while self.number > 0:
+                    Beep(freq, time)
+                    self.number -= 1
+                print('Ended')
+            except KeyboardInterrupt:
+                print('You exited the program.')
+            except Exception as ex:
+                print(str(ex))
+        else:
+            print('The arguments are not correct.')
+
+    def play_music(self) -> None:
+        """Play random music by generating random frequencies and durations."""
+        if isinstance(self.number, int) and self.number > 0:
+            try:
+                while self.number > 0:
+                    Beep(randint(100, 1200), randint(300, 500))
+                    self.number -= 1
+                print('Ended')
+            except KeyboardInterrupt:
+                print('You exited the program.')
+            except Exception as ex:
+                print(str(ex))
+        else:
+            print('Number is not an integer.')
+
+if __name__ == "__main__":
+    obj = Bib(20)
+    obj.play_music()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Frequent-MiniProj
+# Bib Sound Player
+
+## Overview
+
+The **Bib Sound Player** is a simple Python application that generates beep sounds using the winsound library. It allows users to play a specified number of beeps at given frequencies and durations, or to play random beeps with varying frequencies and durations.
+
+## Features
+
+- Play a specified number of beeps with defined frequency and duration.
+- Generate random beep sounds with varying frequencies and durations.
+- Handle user interruptions gracefully.
+
+## Requirements
+
+- Python 3.x
+- Windows operating system (as it uses the winsound library)
